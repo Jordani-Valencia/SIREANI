4c8d3142c2d41b9ae42a1ce352c1728dc9c153af
rubenSValencia <rubensv_1523@hotmail.com>
2018-09-22 12:03:08 -0500

    modelo login

---
+++ application/models/MLogin.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class MLogin extends CI_Model
+{
+  function __construct()
+  {
+      parent::__construct();
+      $this->load->database();
+  }
+
+  // function InsertaRegistro($data){
+  //   $this->db->Insert('login',array('idLogin'=>NULL, 'usuario'=>$data['usuario'], 'contrasena'=>$data['contrasena'] ));
+  // }
+
+  function seleccionUsuario ($data){
+
+    $this->db->where('usuario',$data['username']);
+    $this->db->where('contrasena',$data['pass']);
+    $query =$this->db->get('login');
+
+    if ($query->num_rows()>0) {
+      return 'pantallas/principalAdmin';
+    }else{
+      return '';
+    }
+  }
+}
+
+
+ ?>
