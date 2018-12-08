766b078ac79e772b2b0f0e8a86cd79997bb6fa22
jordani <jordani2010@live.com>
2018-12-08 11:30:49 -0600

    2 y footer

--- application/views/pantallas/footer.php
+++ application/views/pantallas/footer.php
@@ -2,12 +2,8 @@
 
 	<script type="text/javascript" src="<?php echo base_url();?>/assets/js/jquery-3.3.1.min.js"></script>
 
-	<script type="text/javascript" src="<?php echo base_url();?>/assets/js/jquery-3.3.1.min.js"></script>
-
 	<script type="text/javascript" src="<?php echo base_url();?>/assets/js/init.js"></script>
 	<script type="text/javascript" src="<?php echo base_url();?>/assets/js/materialize.min.js"></script>
-	<script type="text/javascript">
-		base="<?php echo base_url()?>";
-	</script>
+	
 </body>
 </html>
